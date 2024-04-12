3fe0b633016ebd413e730f42f59f37aac892d79e
Govind Kumar <govind.kumar@tech9.com>
2024-04-12 19:42:29 +0530

    proxy server code

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+var cors = require('cors')
+const app = express();
+app.use(cors());
+const { createProxyMiddleware } = require('http-proxy-middleware');
+app.use('/', createProxyMiddleware({ 
+    target: 'http://172.31.50.13:9200', //original url
+    changeOrigin: true, 
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
+app.listen(5000);
